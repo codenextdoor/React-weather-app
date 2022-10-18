8d876b3aefdcad70834b9a61d3957050d2569a85
Sulav Pandey <xuluv911@gmail.com>
2022-10-18 20:49:32 +0545

    added search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Inputs from "./components/Inputs";
 import TopButtons from "./components/TopButtons";
 const App = () => {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ">
       <TopButtons />
+      <Inputs />
     </div>
   );
 };
--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
 
 const Inputs = () => {
-  return <div>Inputs</div>;
+  return (
+    <div className="flex flex-row justify-center my-6">
+      <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+        <input
+          type="text"
+          className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize"
+          placeholder="search for city..."
+        />
+
+        <UilSearch
+          size={25}
+          className="text-white cursor-pointer transition ease-out hover:scale-125"
+        />
+        <UilLocationPoint
+          size={25}
+          className="
+          text-white
+          cursor-pointer
+          transition
+          ease-out
+          hover:scale-125"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Inputs;
