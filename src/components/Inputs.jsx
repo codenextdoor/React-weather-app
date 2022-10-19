fc5d93e5e862b14768acce7a07c10900c817faaa
Sulav Pandey <xuluv911@gmail.com>
2022-10-19 11:08:32 +0545

    location commit

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -7,10 +7,9 @@ const Inputs = () => {
       <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
         <input
           type="text"
-          className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize"
+          className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
           placeholder="search for city..."
         />
-
         <UilSearch
           size={25}
           className="text-white cursor-pointer transition ease-out hover:scale-125"
@@ -24,6 +23,15 @@ const Inputs = () => {
           ease-out
           hover:scale-125"
         />
+      </div>
+      <div className="flex flex-row w-1/4 items-center justify-center">
+        <button name="metric" className="text-xl text-white font-light">
+          °C
+        </button>
+        <p className="text-white px-1">|</p>
+        <button name="imperial" className="text-xl text-white font-light mx-0">
+          °F
+        </button>
       </div>
     </div>
   );
