6377054de0b7a629ed01200853a86dd7f87dcba2
Sulav Pandey <xuluv911@gmail.com>
2022-10-19 20:13:30 +0545

    weather service commit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import TemperatureAndDetails from "./components/TemperatureAndDetails";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TopButtons from "./components/TopButtons";
 const App = () => {
+  const fetchWeather = async () => {};
+
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ">
       <TopButtons />
       <Inputs />
       <TimeAndLocation />
       <TemperatureAndDetails />
-      <Forecast />
+      <Forecast title="Hourly forecast" />
+      <Forecast title="Daily forecast" />
     </div>
   );
 };
---
+++ src/services/weatherService.js
@@ -0,0 +1,13 @@
+const API_KEY = "53a3e1968e8317562e628d4dd248a68f";
+const BASE_URL = " https://api.openweathermap.org/data/2.5";
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => data);
+};
+
+export default getWeatherData;
