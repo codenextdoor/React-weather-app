f28f42a14dab7709ab6e5c6439f10f5d1a8ea664
Sulav Pandey <xuluv911@gmail.com>
2022-10-19 13:11:54 +0545

    temp details

--- src/components/TemperatureAndDetails.jsx
+++ src/components/TemperatureAndDetails.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {
+  UilArrowUp,
+  UilArrowDown,
   UilTemperature,
   UilTear,
   UilWind,
@@ -15,14 +17,35 @@ const TemperatureAndDetails = () => {
       </div>
       <div className="flex flex-row items-center justify-center justify-between py-3">
         <img
-          className="w-20"
+          className="w-20 mr-3"
           src="http://openweathermap.org/img/wn/01d@2x.png"
           alt=""
         />
-        <p className="text-5xl text-white">34°</p>
+        <p className="text-5xl text-white ml-14">34°</p>
         <div className="flex flex-col space-y-2">
-          <div className="flex font-light text-sm items-center justify-center"></div>
+          <div className="flex font-light text-sm items-center justify-center ml-5">
+            <UilTemperature size={18} className="mr-1 text-white" />
+            <p className="text-white">Realfeel: </p>{" "}
+            <span className="text-white ml-1 font-medium"> 32°</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center ml-5">
+            <UilTear size={18} className="mr-1 text-white" />
+            <p className="text-white">Humidity: </p>{" "}
+            <span className="text-white ml-1 font-medium"> 65%</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center ml-5">
+            <UilWind size={18} className="mr-1 text-white" />
+            <p className="text-white">Wind: </p>{" "}
+            <span className="text-white ml-1 font-medium"> 11 km/h</span>
+          </div>
         </div>
+      </div>
+
+      <div className="flex flex-row items-center justify-center space-x-2 text-white text-sm py-3">
+        <UilSun />
+        <p className="font-light">
+          Rise: <span>06:45am</span>
+        </p>
       </div>
     </div>
   );
